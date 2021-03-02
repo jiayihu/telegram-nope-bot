11e48db98be86955da4fd4e1f605340a0ea95c48
Jiayi Hu <jiayi.ghu@gmail.com>
2021-03-02 22:16:35 +0100

    Support asking current NOPE value

--- index.ts
+++ index.ts
@@ -29,6 +29,24 @@ const TICKERS = [
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
 bot.hears('Hi', (ctx) => ctx.reply('Hey there'));
 
+bot.command('now', (ctx) => {
+  const ticker = ctx.message.text.replace('/now ', '');
+
+  if (ticker === '/now') {
+    return ctx.reply(`Wrong command format. Correct format is '/now GME'`);
+  }
+
+  if (!TICKERS.includes(ticker)) {
+    return ctx.reply(`Ticker ${ticker} not in the list`);
+  }
+
+  requestNOPE(ticker).then(([nope, price]) => {
+    const msg = buildMsg(ticker, nope, price);
+
+    ctx.reply(msg);
+  });
+});
+
 bot.command('track', (ctx) => {
   const message = ctx.message.text.replace('/track ', '');
 
@@ -43,33 +61,14 @@ bot.command('track', (ctx) => {
     return ctx.reply(`Ticker ${ticker} not in the list`);
   }
 
-  const now = new Date();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-  const date = `${month}-${day}-${now.getFullYear()}`;
-  const url = `https://nopechart.com/cache/${ticker}_${date}.json?=${now.getTime()}`;
-
   const update = () =>
-    fetch(url)
-      .then((response) => response.json())
-      .then((ticks) => {
-        const last = ticks[ticks.length - 1];
-        const { nope, price } = last;
-        const nopePretty = nope.toFixed(2);
-        const pricePretty = price.toFixed(2);
+    requestNOPE(ticker).then(([nope, price]) => {
+      if (Math.abs(nope) >= threshold) {
+        const msg = buildMsg(ticker, nope, price);
 
-        console.log(ticker, nope, price);
-
-        // const trackMsg = `${ticker} NOPE: ${nopePretty}, price: ${pricePretty}`;
-        // ctx.reply(trackMsg, {
-        //   disable_notification: true,
-        // });
-
-        if (Math.abs(nope) >= threshold) {
-          const alertMsg = `${ticker} NOPE: ${nopePretty} vs ${threshold}, price: ${pricePretty}`;
-          ctx.reply(alertMsg);
-        }
-      });
+        ctx.reply(msg);
+      }
+    });
 
   setInterval(() => update(), 30 * 1000); // 30s
 
@@ -83,6 +82,30 @@ bot.launch();
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+function requestNOPE(ticker: string) {
+  const now = new Date();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const date = `${month}-${day}-${now.getFullYear()}`;
+  const url = `https://nopechart.com/cache/${ticker}_${date}.json?=${now.getTime()}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((ticks) => {
+      const last = ticks[ticks.length - 1];
+      const { nope, price } = last;
+
+      return [nope, price];
+    });
+}
+
+function buildMsg(ticker: string, nope: number, price: number) {
+  const nopePretty = nope.toFixed(2);
+  const pricePretty = price.toFixed(2);
+
+  return `${ticker} NOPE: ${nopePretty}, price: ${pricePretty}`;
+}
 
 function requestPage(url: string) {
   return fetch(url).then((response) => response.text());

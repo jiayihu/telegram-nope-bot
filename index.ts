646b6bff825f41d5860cd3aa1d308fb03cd08835
Jiayi Hu <jiayi.ghu@gmail.com>
2021-03-10 17:36:10 +0100

    Improve error handling with fp-ts

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 import { Context, Telegraf } from 'telegraf';
 import fetch from 'node-fetch';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/lib/Option';
+import { pipe } from 'fp-ts/lib/function';
+import { Newtype, iso } from 'newtype-ts';
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
+interface Ticker extends Newtype<{ readonly Ticker: unique symbol }, string> {}
+const isoTicker = iso<Ticker>();
 
 const TICKERS = [
   'AAPL',
@@ -28,8 +35,12 @@ const TICKERS = [
   'TSLA',
 ];
 
+function validateTicker(ticker: string): O.Option<Ticker> {
+  return TICKERS.includes(ticker) ? O.some(isoTicker.wrap(ticker)) : O.none;
+}
+
 type TrackRecord = {
-  ticker: string;
+  ticker: Ticker;
   token: NodeJS.Timeout;
 };
 
@@ -37,7 +48,7 @@ let trackingRecords: Array<TrackRecord> = [];
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN!);
 
-bot.command('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears(['hi', 'Hi'], (ctx) => ctx.reply('Hey there'));
 
 bot.command('now', (ctx) => {
   const ticker = ctx.message.text.replace('/now ', '');
@@ -46,53 +57,75 @@ bot.command('now', (ctx) => {
     return ctx.reply(`Wrong command format. Correct format is '/now GME'`);
   }
 
-  if (!TICKERS.includes(ticker)) {
-    return ctx.reply(`Ticker ${ticker} not in the list`);
-  }
+  pipe(
+    validateTicker(ticker),
+    O.fold(
+      () => {
+        ctx.reply(`Ticker ${ticker} not in the list`);
+      },
+      (ticker) => {
+        requestNOPE(ticker)
+          .then(([nope, price]) => {
+            const msg = buildMsg(ticker, nope, price);
 
-  requestNOPE(ticker)
-    .then(([nope, price]) => {
-      const msg = buildMsg(ticker, nope, price);
-
-      ctx.reply(msg);
-    })
-    .catch((e) => handleNOPEError(ctx, e));
+            ctx.reply(msg);
+          })
+          .catch((e) => replyError(ctx, e));
+      },
+    ),
+  );
 });
 
 bot.command('track', (ctx) => {
   const message = ctx.message.text.replace('/track ', '');
 
   if (message === '/track') {
-    return ctx.reply(`Wrong command format. Correct format is '/track GME'`);
+    return ctx.reply(`Wrong command format. Correct format is '/track GME 30'`);
   }
 
   const [ticker, thresholdAbs] = message.trim().split(' ');
   const threshold = Math.abs(Number(thresholdAbs));
 
-  if (!TICKERS.includes(ticker)) {
-    return ctx.reply(`Ticker ${ticker} not in the list`);
-  }
+  pipe(
+    validateTicker(ticker),
+    E.fromOption(() => `Ticker ${ticker} not in the list`),
+    E.chain((ticker) => {
+      const record = trackingRecords.find((r) => r.ticker === ticker);
 
-  if (trackingRecords.find((r) => r.ticker === ticker)) {
-    return ctx.reply(`Ticker already being tracked`);
-  }
+      return record ? E.left(`Ticker already being tracked`) : E.right(ticker);
+    }),
+    E.chain((ticker) => {
+      return Number.isNaN(threshold)
+        ? E.left(`Invalid threshold ${threshold}. Correct format is '/track GME 30'`)
+        : E.right(ticker);
+    }),
+    E.fold(
+      (error) => {
+        ctx.reply(error);
+      },
+      (ticker) => {
+        const update = () =>
+          requestNOPE(ticker)
+            .then(([nope, price]) => {
+              if (Math.abs(nope) >= threshold) {
+                const msg = buildMsg(ticker, nope, price);
 
-  const update = () =>
-    requestNOPE(ticker)
-      .then(([nope, price]) => {
-        if (Math.abs(nope) >= threshold) {
-          const msg = buildMsg(ticker, nope, price);
+                ctx.reply(msg);
+              }
+            })
+            .catch((e) => {
+              replyError(ctx, e);
+              untrack(ticker);
+            });
 
-          ctx.reply(msg);
-        }
-      })
-      .catch((e) => handleNOPEError(ctx, e));
+        let token = setInterval(() => update(), 30 * 1000); // 30s
+        trackingRecords = [...trackingRecords, { ticker, token }];
 
-  let token = setInterval(() => update(), 30 * 1000); // 30s
-  trackingRecords = [...trackingRecords, { ticker, token }];
-
-  ctx.reply(`Tracking ticker ${ticker} with threshold ${threshold}`);
-  update();
+        ctx.reply(`Tracking ticker ${ticker} with threshold ${threshold}`);
+        update();
+      },
+    ),
+  );
 });
 
 bot.command('untrack', (ctx) => {
@@ -102,20 +135,19 @@ bot.command('untrack', (ctx) => {
     return ctx.reply(`Wrong command format. Correct format is '/untrack GME'`);
   }
 
-  if (!TICKERS.includes(ticker)) {
-    return ctx.reply(`Ticker ${ticker} not in the list`);
-  }
-
-  let record = trackingRecords.find((r) => r.ticker === ticker);
-
-  if (!record) {
-    return ctx.reply(`Ticker not tracked`);
-  }
-
-  trackingRecords = trackingRecords.filter((r) => r.ticker !== ticker);
-  clearInterval(record.token);
-
-  ctx.reply(`${ticker} untracked`);
+  pipe(
+    validateTicker(ticker),
+    E.fromOption(() => `Ticker ${ticker} not in the list`),
+    E.chain((ticker) => E.fromOption(() => `Ticker not tracked`)(untrack(ticker))),
+    E.fold(
+      (error) => {
+        ctx.reply(error);
+      },
+      () => {
+        ctx.reply(`${ticker} untracked`);
+      },
+    ),
+  );
 });
 
 bot.launch();
@@ -124,7 +156,7 @@ bot.launch();
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-function requestNOPE(ticker: string) {
+function requestNOPE(ticker: Ticker) {
   const now = new Date();
   const month = String(now.getMonth() + 1).padStart(2, '0');
   const day = String(now.getDate()).padStart(2, '0');
@@ -135,6 +167,10 @@ function requestNOPE(ticker: string) {
   return fetch(url)
     .then((response) => response.json())
     .then((ticks) => {
+      if (!ticks.length) {
+        throw new Error('No data available');
+      }
+
       const last = ticks[ticks.length - 1];
       const { nope, price } = last;
 
@@ -142,11 +178,22 @@ function requestNOPE(ticker: string) {
     });
 }
 
-function handleNOPEError(ctx: Context, error: any) {
+function replyError(ctx: Context, error: any) {
   ctx.reply(`Error requesting NOPE: ${error}`);
 }
 
-function buildMsg(ticker: string, nope: number, price: number) {
+function untrack(ticker: Ticker): O.Option<TrackRecord> {
+  let record = trackingRecords.find((r) => r.ticker === ticker);
+
+  if (!record) return O.none;
+
+  trackingRecords = trackingRecords.filter((r) => r.ticker !== ticker);
+  clearInterval(record.token);
+
+  return O.some(record);
+}
+
+function buildMsg(ticker: Ticker, nope: number, price: number) {
   const nopePretty = nope.toFixed(2);
   const pricePretty = price.toFixed(2);
 
